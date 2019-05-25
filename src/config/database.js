aee56a2a5b92b315f4f57ae97de71dc45d0a1c22
Kelechi Nwosu <kelechinwosu@ymail.com>
2019-05-25 07:48:01 +0100

    preparing deployment

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,6 @@
 import Sequelize from "sequelize";
 import { MYSQL_DATABASE, MYSQL_USERNAME, MYSQL_PASSWORD, ENV } from "./secrets";
+console.log(MYSQL_PASSWORD);
 const sequelize = new Sequelize(
   MYSQL_DATABASE,
   MYSQL_USERNAME,
